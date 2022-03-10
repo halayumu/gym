dbf382e2b0831db64cd6d888206763991dc3ee22
halayumu <ookamiayumuwww@gmail.com>
2022-03-10 16:40:10 +0900

    test入力フォーム呼び出し

--- express_mysql/app.js
+++ express_mysql/app.js
@@ -5,34 +5,35 @@ const port = 3000
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-const mysql = require('mysql');
+// const mysql = require('mysql');
 
-const con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'gym'
-});
-
-con.connect(function(err) {
-    if (err) throw err;
-    console.log('Connected');
-});
+// const con = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'gym'
+// });
 
 // con.connect(function(err) {
-//     // if (err) throw err;
+//     if (err) throw err;
 //     console.log('Connected');
+// });
+
+// app.get('/', (request, response) => {
 //     const sql = "select * from items"
 //     con.query(sql, function(err, result, fields) {
-//         // if (err) throw err;
-//         console.log(result)
+//         if (err) throw err;
+//         response.send(result)
 //     });
 // });
 
-app.get('/', (request, response) => {
-    const sql = "select * from items"
-    con.query(sql, function(err, result, fields) {
-        if (err) throw err;
-        response.send(result)
-    });
-});
+// const sql = "INSERT INTO items(name) VALUES('kevin')"
+
+// con.query(sql, function(err, result, fields) {
+//     if (err) throw err;
+//     console.log(result)
+// }
+
+
+const path = require('path');
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../tpl/signUp.html')))
