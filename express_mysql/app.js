978bbd79cb1ce0e73a906d030ada83dfb0d3837c
halayumu <ookamiayumuwww@gmail.com>
2022-03-10 17:58:00 +0900

    パッケージ追加

--- express_mysql/app.js
+++ express_mysql/app.js
@@ -1,39 +1,59 @@
+// const express = require('express')
+// const app = express()
+// const port = 3000
+
+
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+//----[mysql処理]----//
+// // const mysql = require('mysql');
+
+// // const con = mysql.createConnection({
+// //     host: 'localhost',
+// //     user: 'root',
+// //     password: '',
+// //     database: 'gym'
+// // });
+
+// // con.connect(function(err) {
+// //     if (err) throw err;
+// //     console.log('Connected');
+// // });
+
+// // app.get('/', (request, response) => {
+// //     const sql = "select * from items"
+// //     con.query(sql, function(err, result, fields) {
+// //         if (err) throw err;
+// //         response.send(result)
+// //     });
+// // });
+
+// // const sql = "INSERT INTO items(name) VALUES('kevin')"
+
+// // con.query(sql, function(err, result, fields) {
+// //     if (err) throw err;
+// //     console.log(result)
+// // }
+
+/////////////////////////////////
+
+//----[入力フォーム表示処理]----//
+// const path = require('path');
+// app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../tpl/signUp.html')))
+
+
+//----[フォームの値を取得表示処理]----//
+const path = require('path')
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-// const mysql = require('mysql');
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// const con = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: '',
-//     database: 'gym'
-// });
-
-// con.connect(function(err) {
-//     if (err) throw err;
-//     console.log('Connected');
-// });
-
-// app.get('/', (request, response) => {
-//     const sql = "select * from items"
-//     con.query(sql, function(err, result, fields) {
-//         if (err) throw err;
-//         response.send(result)
-//     });
-// });
-
-// const sql = "INSERT INTO items(name) VALUES('kevin')"
-
-// con.query(sql, function(err, result, fields) {
-//     if (err) throw err;
-//     console.log(result)
-// }
-
-
-const path = require('path');
+// const path = require('path');
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../tpl/signUp.html')))
+
+app.post('/', (req, res) => res.send(req.body)) //追加
